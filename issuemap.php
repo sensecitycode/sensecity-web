83b54ce476ab6a3f1201d8609ee599cdfdc6c08f
kbakoulias <kostas.bakoulias@gmail.com>
2016-04-08 11:24:16 +0300

    ok

--- issuemap.php
+++ issuemap.php
@@ -238,23 +238,6 @@
 				}
 			});
 			
-			
-				
-				
-				
-			
-				
-				
-		
-		
-		
-				/*$('a.page-scroll').bind('click', function(event) {
-					var $anchor = $(this);
-					$('html, body').stop().animate({
-						scrollTop: ($($anchor.attr('href')).offset().top - 50)
-					}, 1250, 'easeInOutExpo');
-					event.preventDefault();
-				});*/
 
 			</script>
 			
