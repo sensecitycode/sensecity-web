c8d3054baeb22851873a893f9ae89ca821431183
ctranoris <tranoris@gmail.com>
2016-07-09 00:26:06 +0300

    bugzilla search

--- overviewctrl.js
+++ overviewctrl.js
@@ -441,7 +441,7 @@ appControllers
 								var solutionsParam =
 								{
 										"method": "Bug.search",
-										"params": [{"product": "Δημος Πατρέων","order":"bug_id DESC","status":"RESOLVED","f1":"resolution","o1":"changedafter","v1":"2016-01-01","include_fields":["id"]}],
+										"params": [{"product": "Δημος Πατρέων","order":"bug_id DESC","status":"RESOLVED","resolution": "FIXED","f1":"resolution","o1":"changedafter","v1":"2016-01-01","include_fields":["id"]}],
 										"id": 1
 								};
 								BugService.search(solutionsParam, function(result) {
