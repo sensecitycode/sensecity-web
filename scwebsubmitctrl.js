14e3406162559e19753bb88cb4ee165cb1165524
kbakoulias <kostas.bakoulias@gmail.com>
2016-11-07 12:17:38 +0200

    ok

--- scwebsubmitctrl.js
+++ scwebsubmitctrl.js
@@ -209,7 +209,7 @@ appControllers.controller('scWebSubmit',  [ '$scope', '$rootScope', '$log', '$lo
 						data : txtpost 
 						
 					}).success(function() {
-				$location.path("/");
+				$location.path("/eponymous");
 			});
 		}
 
