23ff85ba035837dabd3c4708137be29e51a99cc3
kbakoulias <kostas.bakoulias@gmail.com>
2016-11-21 15:25:32 +0200

    ok

--- overviewctrl.js
+++ overviewctrl.js
@@ -246,7 +246,7 @@ appControllers
 					     var popup = marker3.getPopup();
 					     // marker3.bindPopup("Loading...");
 							 var issue_name;
-						console.log("issueID:marker3.options.issue_id============================================================"+issueID:marker3.options.issue_id)
+						console.log("issueID:marker3.options.issue_id============================================================"+issueID:marker3.options.issue_id);
 					     Issue2MapService.query({issueID:marker3.options.issue_id}, function(resp) {
 							console.log("resp =============-------------------->>>>>>>>>>"+JSON.stringify(resp));
 							console.log("resp[0] =============-------------------->>>>>>>>>>"+resp[0].issue);
