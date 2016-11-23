a0aafb1bf5dfb58746fa5c3a2fe787828847c08c
apalladinos <numgroup@150.140.250.162>
2016-11-23 16:46:25 +0200

    ok

--- admin/adminsrvs.js
+++ admin/adminsrvs.js
@@ -5,185 +5,198 @@ var appServices = angular.module('adminapp.adminsrvs', []);
 //  function($cookieStore) {
 //  }]);
 
-appServices.factory('EndPointService', function() {
-	  return {
-		  bugzillaURL: "http://api.sense.city:4000/admin/bugs/search",
-			APIURL: "http://api.sense.city:4000"
-	  };
+appServices.factory('EndPointService', function () {
+    return {
+        bugzillaURL: "http://api.sense.city:4001/bugs/search",
+        APIURL: "http://api.sense.city:4000"
+    };
 });
 
-appServices.factory('BugService', function ( $resource,$cookieStore, EndPointService) {
+appServices.factory('BugService', function ($resource, $cookieStore, EndPointService) {
     return $resource(
-        EndPointService.bugzillaURL,
-        null,
-        {
-          search: {
-            method: 'POST',
-            headers:{'Content-Type':'application/json','x-uuid': $cookieStore.get('uuid'),'x-role': $cookieStore.get('role')},
-            isArray: true,
-            withCredentials: false
-          }
-        }
+            EndPointService.bugzillaURL,
+            null,
+            {
+                search: {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json','x-uuid': $cookieStore.get('uuid'),'x-role': $cookieStore.get('role')},
+                    isArray: true,
+                    withCredentials: false
+                }
+            }
     );
 });
 
-appServices.factory('Issue2MapService', function ( $resource, EndPointService) {
+appServices.factory('Issue2MapService', function ($resource, EndPointService) {
     // console.log("DisplayIssues");
-    return $resource(EndPointService.APIURL+'/api/fullissue/:issueID',
-        {issueID:'@id'}
-			);
+    return $resource(EndPointService.APIURL + '/api/fullissue/:issueID',
+            {issueID: '@id'}
+    );
 });
 
-appServices.factory('FixPoints2MapService', function ( $resource, EndPointService) {
+appServices.factory('FixPoints2MapService', function ($resource, EndPointService) {
     // console.log("DisplayFixPoints");
-    return $resource(EndPointService.APIURL+'/fix_point/:long/:lat/50/:type',
-        {
-					long:'@long',
-					lat:'@lat',
-					type:"@type"
-				}
-			);
+    return $resource(EndPointService.APIURL + '/fix_point/:long/:lat/50/:type',
+            {
+                long: '@long',
+                lat: '@lat',
+                type: "@type"
+            }
+    );
 });
 
-appServices.factory('FixPointsMarkerService', function() {
+appServices.factory('FixPointsMarkerService', function () {
     return {
-        icon: function(fixPoint) {
-					var icon;
-					if (fixPoint.type=="garbage")
-					{
-						switch(fixPoint.notes[0].ANAKIKLOSI){
-							case "0":
-								icon = "staticGarbage";
-								break;
-							case "1":
-								icon = "staticGarbageRecycle";
-								break;
-							default:
-								break;
-						}
-					}
-					else
-					{
-						icon = "staticLighting";
-					}
-					return icon;
+        icon: function (fixPoint) {
+            var icon;
+            if (fixPoint.type == "garbage")
+            {
+                switch (fixPoint.notes[0].ANAKIKLOSI) {
+                    case "0":
+                        icon = "staticGarbage";
+                        break;
+                    case "1":
+                        icon = "staticGarbageRecycle";
+                        break;
+                    default:
+                        break;
+                }
+            } else
+            {
+                icon = "staticLighting";
+            }
+            return icon;
         }
     };
 });
 
-appServices.factory('ToGrService', function() {
+appServices.factory('ToGrService', function () {
     return {
-        issueName: function(issue_name) {
-					switch(issue_name){
-						case "garbage":
-							issue_name = "Πρόβλημα Καθαριότητας";
-							break;
-						case "lighting":
-							issue_name = "Πρόβλημα Φωτισμού";
-							break;
-						case "plumbing":
-							issue_name = "Πρόβλημα Ύδρευσης";
-							break;
-						case "road-contructor":
-							issue_name = "Πρόβλημα Δρόμου/Πεζοδρομίου";
-							break;
-						default:
-							break;
-					}
-					return issue_name;
+        issueName: function (issue_name) {
+            switch (issue_name) {
+                case "garbage":
+                    issue_name = "Πρόβλημα Καθαριότητας";
+                    break;
+                case "lighting":
+                    issue_name = "Πρόβλημα Φωτισμού";
+                    break;
+                case "plumbing":
+                    issue_name = "Πρόβλημα Ύδρευσης";
+                    break;
+                case "road-contructor":
+                    issue_name = "Πρόβλημα Δρόμου/Πεζοδρομίου/Πλατείας";
+                    break;
+                case "green":
+                    issue_name = "Πρόβλημα Πρασίνου";
+                    break;
+                case "protection-policy":
+                    issue_name = "Πρόβλημα Πολιτικής Προστασίας";
+                    break;
+                default:
+                    break;
+            }
+            return issue_name;
         },
 
-				statusTitle: function(status,resolution) {
-					var _status;
-					var _style;
-					var _icon;
-					var _resolution;
-					switch(status){
-						case "CONFIRMED":
-							_status = "Ανοιχτό";
-							_style = {color:'red'};
-							_icon = "glyphicon glyphicon-exclamation-sign";
-							break;
-						case "IN_PROGRESS":
-							_status = "Σε εκτέλεση";
-							_style = {color:'orange'};
-							_icon = "glyphicon glyphicon-question-sign";
-							break;
-						case "RESOLVED":
-							_style = {color:'green'};
-							_icon = "glyphicon glyphicon-ok-sign";
-							switch(resolution){
-								case "FIXED":
-									_resolution = "Αποκατάσταση";
-									break;
-								case "INVALID":
-									_resolution = "Εσφαλμένη Αναφορά";
-									break;
-								case "WONTFIX":
-									_resolution = "Μη αποκατάσταση / Απόρριψη από Δήμο";
-									break;
-								case "DUPLICATE":
-									_resolution = "Έχει ήδη αναφερθεί σε άλλο αίτημα";
-									break;
-							}
-							_status = "Ολοκληρωμένο";
+        statusTitle: function (status, resolution) {
+            var _status;
+            var _style;
+            var _icon;
+            var _resolution;
+            switch (status) {
+                case "CONFIRMED":
+                    _status = "Ανοιχτό";
+                    _style = {color: '#e42c2c'};
+                    _icon = "glyphicon glyphicon-exclamation-sign";
+                    break;
+                case "IN_PROGRESS":
+                    _status = "Σε εκτέλεση";
+                    _style = {color: 'orange'};
+                    _icon = "glyphicon glyphicon-question-sign";
+                    break;
+                case "RESOLVED":
+                    _style = {color: 'green'};
+                    _icon = "glyphicon glyphicon-ok-sign";
+                    switch (resolution) {
+                        case "FIXED":
+                            _resolution = "Αποκατάσταση";
+                            break;
+                        case "INVALID":
+                            _resolution = "Εσφαλμένη Αναφορά";
+                            break;
+                        case "WONTFIX":
+                            _resolution = "Μη αποκατάσταση / Απόρριψη από Δήμο";
+                            break;
+                        case "DUPLICATE":
+                            _resolution = "Έχει ήδη αναφερθεί σε άλλο αίτημα";
+                            break;
+                    }
+                    _status = "Ολοκληρωμένο";
 
-							break;
-						default:
-							break;
-					}
-					return {"status":{"en":status,"gr":_status},"status_style":_style,"status_icon":_icon,"resolution":{"en":resolution,"gr":_resolution}};
+                    break;
+                default:
+                    break;
+            }
+            return {"status": {"en": status, "gr": _status}, "status_style": _style, "status_icon": _icon, "resolution": {"en": resolution, "gr": _resolution}};
 
-				}
+        }
     };
 });
 
-appServices.factory('CommentService', function() {
+appServices.factory('CommentService', function () {
     return {
-				field: function(status) {
-					var field;
-					var comment;
-					switch(status){
-						case "CONFIRMED":
-							// field = "url";
-							break;
-						case "IN_PROGRESS":
-							field = "whiteboard";
-							break;
-						case "RESOLVED":
-							field = "cf_sensecityissue";
-							break;
-						default:
-							break;
-					}
-					return field;
+        field: function (status) {
+            var field;
+            var comment;
+            switch (status) {
+                case "CONFIRMED":
+                    // field = "url";
+                    break;
+                case "IN_PROGRESS":
+                    field = "whiteboard";
+                    break;
+                case "RESOLVED":
+                    field = "cf_sensecityissue";
+                    break;
+                default:
+                    break;
+                    field = "cf_comment";        
 
-				}
+            }
+            return field;
+
+        }
     };
 });
 
-appServices.factory('Tab2BugzillaService', function(){
-	return{
-		issue_type: function(tab) {
-			var type;
-			switch (tab) {
-				case "Γενικά":
-					type="all";
-					break;
-				case "Καθαριότητα":
-					type="garbage";
-					break;
-				case "Φωτισμός":
-					type="lighting";
-					break;
-				case "Ύδρευση":
-					type="plumbing";
-					break;
-				case "Οδόστρωμα":
-					type="road-contructor";
-					break;
-			}
-			return type;
-		}
-	};
+appServices.factory('Tab2BugzillaService', function () {
+    return{
+        issue_type: function (tab) {
+            var type;
+            switch (tab) {
+                case 0:
+                    type = "all";
+                    break;
+                case 1:
+                    type = "garbage";
+                    break;
+                case 2:
+                    type = "lighting";
+                    break;
+                case 3:
+                    type = "road-contructor";
+                    break;
+                case 4:
+                    type = "protection-policy";
+                    break;
+                case 5:
+                    type = "green";
+                    break;
+                case 6:
+                    type = "plumbing";
+                    break;
+            }
+            return type;
+        }
+    };
 });
--- admin/login_controller.js
+++ admin/login_controller.js
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var app = angular.module('login_fo', ['ngCookies']);
+var app = angular.module('login_fo', ['ngCookies'])
+        .constant("config", {"host": "http://api.sense.city", "bugzilla_host": "nam.ece.upatras.gr:80", "port": "4000", "bugzilla_path": "/bugzilla"});
 
 //app.config([
 //  '$httpProvider',
@@ -11,7 +12,7 @@ var app = angular.module('login_fo', ['ngCookies']);
 //    $httpProvider.defaults.withCredentials = true;
 //  }]);
 
-app.controller('login_controller', ['$scope', '$window', '$http','$cookieStore','$location', function ($scope, $window, $http,$cookieStore,$location) {
+app.controller('login_controller', ['$scope', '$window', '$http', '$cookieStore', '$location', 'config', function ($scope, $window, $http, $cookieStore, $location, config) {
         $scope.admin_user = "";
         $scope.lock = "";
         $scope.username_l = "Username";
@@ -19,38 +20,40 @@ app.controller('login_controller', ['$scope', '$window', '$http','$cookieStore',
         $scope.user_focus = function (event) {
 
             $scope.admin_user = {"border": "2px #979ea7 solid", "border-right": "none"};
-        }
+        };
         $scope.lock_focus = function (event) {
             $scope.lock = {"border": "2px #979ea7 solid", "border-right": "none"};
-        }
+        };
         $scope.user_blur = function (event, username) {
             $scope.admin_user = "";
             $scope.username_l = username;
-        }
+        };
         $scope.lock_blur = function (event, password) {
             $scope.lock = "";
             $scope.password_l = password;
-        }
+        };
         $scope.authenticate_us = function (event) {
             var domain = $location.host().split(".");
-            var parameter = JSON.stringify({username: $scope.username_l, password: $scope.password_l, city: domain[0]});
-            $http.post('http://api.sense.city:4000/dashboard', parameter).success(
-                    function (response, status, headers, config) {
-                        response = response.split(';');
-                        if (response != "failure") {
-                            $cookieStore.put('city',response[0]);
-                            $cookieStore.put('role',response[1]);
-                            $cookieStore.put('department',response[2]);
-                            $cookieStore.put('email',response[3]);
-                            $cookieStore.put('uuid',response[4]);
-                            $cookieStore.put('username',response[5]);
-                            $window.location.href = "/admin/admin.html";
-                        } else {
-                            $window.alert("Wrong credentials!");
-                        }
-                    }).error(
-                    function (response) {
-                        $window.alert("failure");
-                    });
-        }
+            var parameter = {username: $scope.username_l, password: $scope.password_l, city: domain};                       
+            $http.post('http://' + config.host + ':' + config.port + '/dashboard', parameter).success(
+                                function (response, status, headers, cnfg) {
+                                    response = response.split(';');
+                                    if (response != "failure") {
+                                        $cookieStore.put('city', response[0]);
+                                        $cookieStore.put('role', response[1]);
+                                        $cookieStore.put('department', response[2]);
+                                        $cookieStore.put('email', response[3]);
+                                        $cookieStore.put('uuid', response[4]);
+                                        $cookieStore.put('username', response[5]);
+
+                                        $window.location.href = "/admin/admin.html";
+
+                                    } else {
+                                        $window.alert("Wrong credentials!");
+                                    }
+                                }).error(
+                                function (response) {
+                                    $window.alert("failure");
+                                });
+        };
     }]);
