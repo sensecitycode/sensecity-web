a6b9526d90d636f921d5d2a5bb0767f20faaff46
apalladinos <numgroup@150.140.250.162>
2016-12-01 16:19:20 +0200

    ok

--- admin/adminctrl.js
+++ admin/adminctrl.js
@@ -879,6 +879,7 @@ appControllers.controller('adminController', ['$scope', '$window', '$http', '$co
                         }
                         if ($scope.selectedStatus.gr != panel.status.gr || $scope.selectedComponent != panel.component || panel.comment != $scope.comment || $scope.selectedResolution != panel.resolution || $scope.duplicof != panel.duplicof || $scope.selectedPriority.gr != panel.priority.gr || $scope.selectedSeverity.gr != panel.severity.gr) {
                             $scope.comment = panel.comment;
+                            $window.alert($scope.selectedResolution != panel.resolution);
                             $window.alert(JSON.stringify($scope.selectedResolution));
                             $window.alert(JSON.stringify(panel.resolution));
                             panel.priority = {en: PriorityTagEn.priority_type(seldpriority.gr), gr: seldpriority.gr};
