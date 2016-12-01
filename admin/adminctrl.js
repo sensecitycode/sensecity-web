f2554b47db755c5f4d99fe0227b3573e77f3a35f
apalladinos <numgroup@150.140.250.162>
2016-12-01 11:56:30 +0200

    ok

--- admin/adminctrl.js
+++ admin/adminctrl.js
@@ -882,7 +882,8 @@ appControllers.controller('adminController', ['$scope', '$window', '$http', '$co
                                     }
                                     $scope.selectedStatus = panel.status;
                             }else{
-                              $scope.comment = panel.comment;  
+                              $scope.comment = panel.comment; 
+                              $scope.selectedStatus = panel.status;
                             }
                         }
                     } else if ($scope.selectedStatus.gr == 'Ολοκληρωμένο') {
@@ -908,7 +909,10 @@ appControllers.controller('adminController', ['$scope', '$window', '$http', '$co
                             if( panel.comment == undefined){
                                 panel.comment = "undefined";
                             }
+                            $window.alert(panel.comment);
+                            $window.alert($scope.comment);
                             if (panel.comment != $scope.comment) {
+                                    $window.alert("mphke!");
                                     $scope.comment = panel.comment;
                                     update();
                                     if ((panel.status.gr == 'Σε εκτέλεση' && panel.component != $scope.component) || (panel.status.gr == 'Ολοκληρωμένο' && panel.component != $scope.component && (($scope.closedissues == false && $scope.allclosedissues == false) || ($scope.closedissues == true && panel.component != $scope.component)))) {
@@ -920,7 +924,8 @@ appControllers.controller('adminController', ['$scope', '$window', '$http', '$co
                                     }
                                     $scope.selectedStatus = panel.status;
                             }else{
-                              $scope.comment = panel.comment;  
+                              $scope.comment = panel.comment;
+                              $scope.selectedStatus = panel.status;
                             }
                         }
                     }
