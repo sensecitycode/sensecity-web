81b930261289cb79d32689269dde11e812a56e1a
apalladinos <numgroup@150.140.251.174>
2017-05-29 16:43:42 +0300

    ok

--- admin/adminctrl.js
+++ admin/adminctrl.js
@@ -807,7 +807,6 @@ appControllers.controller('adminController', ['$scope', '$rootScope', '$window',
                             "content": $rootScope.Variables.depUserContent[dep_index],
                             "icon": $rootScope.Variables.depUserIcons[dep_index]
                         }];
-                    alert(JSON.stringify(department[0]));
                 } else {
                     $scope.tabs = [{
                             "title": "Όλα τα τμήματα",
