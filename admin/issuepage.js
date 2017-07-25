bfd3635565ff763416e807442548da8bca2ea1f4
apalladinos <numgroup@150.140.251.174>
2017-07-25 09:51:46 +0300

    ok

--- admin/issuepage.js
+++ admin/issuepage.js
@@ -42,10 +42,10 @@ app.controller('issuepage_controller', ['$scope', '$rootScope', '$window', '$coo
                 openStreetMap: {
                     name: 'OpenStreetMap',
                     type: 'xyz',
-                    url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                     layerOptions: {
                         showOnSelector: false,
-                        attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+                        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
                     }
                 }
             }
